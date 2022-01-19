8eab4c82b98115742eba586c828a5c2b22676aa6
Aleksandr Elizarov <flashwunb@yandex.ru>
2022-01-19 13:18:40 +0300

    ЗАЛИЛ САЙТ

---
+++ script.js
@@ -0,0 +1,9 @@
+var headerMenu = document.getElementById("headerMenu");
+
+function showMenu() {
+   headerMenu.style.right = "0";
+}
+
+function hideMenu() {
+   headerMenu.style.right = "-60%";
+}
