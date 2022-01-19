93a8e67304d79947108792918f541850e6aa3269
Aleksandr Elizarov <flashwunb@yandex.ru>
2022-01-19 13:41:22 +0300

    Добавил скрипт в html и убрал файл js

--- script.js
+++
@@ -1,9 +0,0 @@
-var headerMenu = document.getElementById("headerMenu");
-
-function showMenu() {
-   headerMenu.style.right = "0";
-}
-
-function hideMenu() {
-   headerMenu.style.right = "-60%";
-}
